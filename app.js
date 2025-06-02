5b7d3f7d0044b89d60b89a04b21e1386cfc1849e
Libin Thankayathil <libinthankayathil@gmail.com>
2025-06-02 15:13:42 +0400

    added sign-up and sign-in pages

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.get("/", function (request, response) {
   response.render('index');
 });
 
+app.get("/auth/signin", function (request, response) {
+  response.render('auth/signin');
+});
+
+app.get("/auth/signup", function (request, response) {
+  response.render('auth/signup');
+});
+
 module.exports = app;
