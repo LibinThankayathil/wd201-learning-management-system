6dbc7a8892849c54ae6da550da02ec13e7fe2410
Libin Thankayathil <libinthankayathil@gmail.com>
2025-06-15 14:34:20 +0400

    minor fixes

--- app.js
+++ app.js
@@ -39,16 +39,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//connect-flash middleware for flash messages
-// app.use(flash());
-// app.use((req, res, next) => {
-//   res.locals.success_msg = req.flash("success_msg");
-//   res.locals.error_msg = req.flash("error_msg");
-//   res.locals.error = req.flash("error"); // Often used with Passport
-//   next();
-// });
-
-
 // Passport local strategy for authentication
 passport.use(
   new LocalStrategy(
